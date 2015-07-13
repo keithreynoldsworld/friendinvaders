027e64af4ef1b212a65dd2cf090ce3d3154b3060
Keith Reynolds <keithreynoldsworld@gmail.com>
2015-07-13 11:32:38 -0500

    parse connected

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var React = require('react');
 var $ = require('jquery');
 var Backbone = require('backbone');
 var killListHolder = {happy:"sad"};
+var Parse = require('parse-browserify');
 var klist = {};
 var klistFINAL = {};
 klistFINAL.list = [];
@@ -27,13 +28,33 @@ var App = Backbone.Router.extend({
 		"levelthree": "levelthree",
 	},
 	login: function() {
-		$('#leveltitle').html("are you ready to MURDER your friends?");
-		$('#phaser-example').html('<button id="gotochoose">kill</button>');
-		$('#gotochoose').on('click', function(e){
-			e.preventDefault;
-			console.log('button works');
-			myRouter.navigate('choosefriendstokill', {trigger: true});
-		});
+		// Initialize Parse
+  		
+    	
+					
+				$('#leveltitle').append("<button id='LO'>Logout from facebook</button>");
+				$('#leveltitle').append("are you ready to MURDER your friends?");
+				$('#phaser-example').html('<button id="gotochoose">kill</button>');
+				$('#LO').on('click', function(e){
+					e.preventDefault;
+					FB.logout(function(response) {
+  						// user is now logged out
+					});
+				});
+				$('#gotochoose').on('click', function(e){
+					e.preventDefault;
+					console.log('button works');
+					myRouter.navigate('choosefriendstokill', {trigger: true});
+				});
+
+				
+		
+		
+
+        // Run code after the Facebook SDK is loaded.
+  
+
+  
 		// React.render(
 		// 	<LoginComponent user={user} myRouter={myRouter} />,
 		// 	document.getElementById("container"));
@@ -57,6 +78,7 @@ var App = Backbone.Router.extend({
   			console.log(response);
   			myProfilePic.house = response;
   		});
+  		
   		
 		//displayFriends();
 
@@ -126,84 +148,6 @@ Backbone.history.start();
 
 console.log('application running');
 
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
-
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '120671521602697',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.3' // use version 2.2
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
 function displayFriends(){
 
 
---
+++ scripts/models/updateUserGetFriendsList.js
@@ -0,0 +1,1 @@
+updateUserGetFriendsList.js
